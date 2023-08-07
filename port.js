a3ca97129c3ce51c520a13044e7aac3c645cb77a
Jmenzies722 <Jmenzies722@gmail.com>
2023-08-07 17:08:43 -0400

    updated

--- port.js
+++ port.js
@@ -37,3 +37,35 @@ function navbarScrollFunction() {
     buttonScrollFunction();
   };
   
+  // Select all links with hashes
+const links = document.querySelectorAll(".nav a[href*='#']");
+
+for (const link of links) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const blockID = link.getAttribute('href');
+        document.querySelector(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+}
+
+const targets = document.querySelectorAll('.animated');
+
+window.addEventListener('scroll', () => {
+    targets.forEach(target => {
+        const visible = elementVisible(target);
+        if (visible) {
+            target.classList.add('fade-in');
+        }
+    });
+});
+
+function elementVisible(el) {
+    const rect = el.getBoundingClientRect();
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+    return (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+}
