dadc1e1ae36da90807578a50f162969d5defbd31
Jmenzies722 <Jmenzies722@gmail.com>
2023-10-07 15:54:19 -0400

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // ... Your existing code for typing animation ...
   let elements = document.querySelectorAll('.section__text__animated');
   let delay = 0;
   
@@ -18,7 +19,46 @@ document.addEventListener("DOMContentLoaded", function() {
       }, delay);
 
       delay += text.length * 150;  // calculate delay based on the current text length
-  });
+  // Event listener for hamburger menu
+  document.querySelector(".hamburger-icon").addEventListener('click', toggleMenu);
+});
+
+let isDragging = false;
+let startX;
+
+document.getElementById('darkModeToggle').addEventListener('mousedown', function(e) {
+  if (e.target.classList.contains('circle')) {
+      isDragging = true;
+      startX = e.clientX;
+  }
+});
+
+document.addEventListener('mousemove', function(e) {
+  if (isDragging) {
+      // You can add more sophisticated logic here to move the circle
+      // while dragging, but for simplicity:
+      let movementX = e.clientX - startX;
+      if (movementX > 10) {  // dragged to the right
+          document.body.classList.add('dark-mode');
+      } else if (movementX < -10) {  // dragged to the left
+          document.body.classList.remove('dark-mode');
+      }
+  }
+});
+
+document.addEventListener('mouseup', function() {
+  if (isDragging) {
+      // If it's a simple click without much dragging, toggle the mode
+      let movementX = event.clientX - startX;
+      if (Math.abs(movementX) < 10) {
+          if (document.body.classList.contains('dark-mode')) {
+              document.body.classList.remove('dark-mode');
+          } else {
+              document.body.classList.add('dark-mode');
+          }
+      }
+      isDragging = false;
+  }
 });
 
 function toggleMenu() {
@@ -27,16 +67,12 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-document.getElementById('darkModeToggle').addEventListener('click', function() {
-  document.body.classList.toggle('dark-mode');
-
-  document.getElementById('darkModeToggle').addEventListener('click', function() {
-    if (document.body.classList.contains('dark-mode')) {
-        document.body.classList.remove('dark-mode');
-    } else {
-        document.body.classList.add('dark-mode');
-    }
 });
 
-});
+document.querySelector('spline-viewer').addEventListener('mousedown', function(e) {
+  e.preventDefault();
+}, { passive: false });
+
+document.querySelector('spline-viewer').addEventListener('touchstart', function(e) {
+  e.preventDefault();
+}, { passive: false });
