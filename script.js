b518cc6bc882fff811366ae15e6ba3c81134e79d
Jmenzies722 <Jmenzies722@gmail.com>
2023-10-09 11:39:59 -0400

    implemented local storage for dark mode

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // Check for the dark mode preference in localStorage when the page loads
+  if (localStorage.getItem('dark-mode') === 'true') {
+      document.body.classList.add('dark-mode');
+  } else {
+      document.body.classList.remove('dark-mode');
+  }
+
   // ... Your existing code for typing animation ...
   let elements = document.querySelectorAll('.section__text__animated');
   let delay = 0;
-  
+
   elements.forEach((element) => {
       let text = element.getAttribute('data-text');
       let index = 0;
@@ -19,60 +26,72 @@ document.addEventListener("DOMContentLoaded", function() {
       }, delay);
 
       delay += text.length * 150;  // calculate delay based on the current text length
+  });
+
   // Event listener for hamburger menu
   document.querySelector(".hamburger-icon").addEventListener('click', toggleMenu);
-});
 
-let isDragging = false;
-let startX;
+  let isDragging = false;
+  let startX;
 
-document.getElementById('darkModeToggle').addEventListener('mousedown', function(e) {
-  if (e.target.classList.contains('circle')) {
-      isDragging = true;
-      startX = e.clientX;
-  }
-});
-
-document.addEventListener('mousemove', function(e) {
-  if (isDragging) {
-      // You can add more sophisticated logic here to move the circle
-      // while dragging, but for simplicity:
-      let movementX = e.clientX - startX;
-      if (movementX > 10) {  // dragged to the right
-          document.body.classList.add('dark-mode');
-      } else if (movementX < -10) {  // dragged to the left
-          document.body.classList.remove('dark-mode');
+  document.getElementById('darkModeToggle').addEventListener('mousedown', function(e) {
+      if (e.target.classList.contains('circle')) {
+          isDragging = true;
+          startX = e.clientX;
       }
-  }
-});
+  });
 
-document.addEventListener('mouseup', function() {
-  if (isDragging) {
-      // If it's a simple click without much dragging, toggle the mode
-      let movementX = event.clientX - startX;
-      if (Math.abs(movementX) < 10) {
-          if (document.body.classList.contains('dark-mode')) {
-              document.body.classList.remove('dark-mode');
-          } else {
-              document.body.classList.add('dark-mode');
+  document.addEventListener('mousemove', function(e) {
+      if (isDragging) {
+          let movementX = e.clientX - startX;
+          if (movementX > 10) {  // dragged to the right
+              setDarkMode(true);
+          } else if (movementX < -10) {  // dragged to the left
+              setDarkMode(false);
           }
       }
-      isDragging = false;
+  });
+
+  document.addEventListener('mouseup', function(e) {
+      if (isDragging) {
+          let movementX = e.clientX - startX;
+          if (Math.abs(movementX) < 10) {
+              toggleDarkMode();
+          }
+          isDragging = false;
+      }
+  });
+
+  document.querySelector('spline-viewer').addEventListener('mousedown', function(e) {
+      e.preventDefault();
+  }, { passive: false });
+
+  document.querySelector('spline-viewer').addEventListener('touchstart', function(e) {
+      e.preventDefault();
+  }, { passive: false });
+
+  function toggleDarkMode() {
+      if (document.body.classList.contains('dark-mode')) {
+          setDarkMode(false);
+      } else {
+          setDarkMode(true);
+      }
+  }
+
+  function setDarkMode(isDark) {
+      if (isDark) {
+          document.body.classList.add('dark-mode');
+          localStorage.setItem('dark-mode', 'true');
+      } else {
+          document.body.classList.remove('dark-mode');
+          localStorage.setItem('dark-mode', 'false');
+      }
+  }
+
+  function toggleMenu() {
+      const menu = document.querySelector(".menu-links");
+      const icon = document.querySelector(".hamburger-icon");
+      menu.classList.toggle("open");
+      icon.classList.toggle("open");
   }
 });
-
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
-});
-
-document.querySelector('spline-viewer').addEventListener('mousedown', function(e) {
-  e.preventDefault();
-}, { passive: false });
-
-document.querySelector('spline-viewer').addEventListener('touchstart', function(e) {
-  e.preventDefault();
-}, { passive: false });
