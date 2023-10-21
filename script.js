2699c48854e81e5e561e2e10fb15ea94c9dd85c0
Jmenzies722 <Jmenzies722@gmail.com>
2023-10-21 13:14:19 -0400

    updated

--- script.js
+++ script.js
@@ -43,62 +43,58 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburgerIcon = document.querySelector(".hamburger-icon");
     hamburgerIcon.addEventListener("click", toggleMenu);
 
-    const profilePic = document.getElementById('darkModeToggle');
-    profilePic.addEventListener('click', function() {
-      let isDark = !document.body.classList.contains('dark-mode');
+    const profilePic = document.getElementById("darkModeToggle");
+    profilePic.addEventListener("click", function () {
+      let isDark = !document.body.classList.contains("dark-mode");
       activateDarkMode(isDark);
     });
 
-    const menuLinks = document.querySelectorAll('.menu-links a');
-    menuLinks.forEach(function(link) {
-      link.addEventListener('click', closeMenu);
+    const menuLinks = document.querySelectorAll(".menu-links a");
+    menuLinks.forEach(function (link) {
+      link.addEventListener("click", closeMenu);
     });
   }
 
   function activateDarkMode(isDark) {
-    const icons = document.querySelectorAll('#socials-container .icon');
+    const icons = document.querySelectorAll("#socials-container .icon");
 
     if (isDark) {
       document.body.classList.add("dark-mode");
       localStorage.setItem("dark-mode", "true");
-      icons.forEach(icon => icon.style.color = '#551a8b'); // purple for dark mode
+      icons.forEach((icon) => (icon.style.color = "#551a8b")); // purple for dark mode
     } else {
       document.body.classList.remove("dark-mode");
       localStorage.setItem("dark-mode", "false");
-      icons.forEach(icon => icon.style.color = 'black'); // black for light mode
+      icons.forEach((icon) => (icon.style.color = "black")); // black for light mode
     }
   }
 
   function toggleMenu() {
-    const menuLinks = document.querySelector('.menu-links');
-    const hamburgerIcon = document.querySelector('.hamburger-icon');
+    const menuLinks = document.querySelector(".menu-links");
+    const hamburgerIcon = document.querySelector(".hamburger-icon");
     const body = document.body;
-  
-    menuLinks.classList.toggle('open');
-    hamburgerIcon.classList.toggle('open');
-    body.classList.toggle('blur'); // Toggle the blur class on the body
+
+    menuLinks.classList.toggle("open");
+    hamburgerIcon.classList.toggle("open");
+    body.classList.toggle("blur"); // Toggle the blur class on the body
   }
 
-  
   document.addEventListener("DOMContentLoaded", function () {
     // Apply ScrollReveal to sections and other major containers
-    ScrollReveal().reveal('section', { 
-        distance: '20px', 
-        origin: 'bottom', 
-        opacity: 0, 
-        duration: 600, 
-        reset: true 
+    ScrollReveal().reveal("section", {
+      distance: "20px",
+      origin: "bottom",
+      opacity: 0,
+      duration: 600,
+      reset: true,
     });
 
-    // If you want more specific animations for inner elements, you can target them as well
-    ScrollReveal().reveal('.section__text', { 
-        distance: '10px', 
-        origin: 'right', 
-        delay: 300,
-        duration: 600, 
-        reset: true 
+    ScrollReveal().reveal(".section__text", {
+      distance: "10px",
+      origin: "right",
+      delay: 300,
+      duration: 600,
+      reset: true,
     });
-    
-    // Add more reveal animations for other elements as required
   });
 });
